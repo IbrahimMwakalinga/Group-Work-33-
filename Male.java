f5d95cac4ebbc2c4755b7936ce1fdfc0b044816d
IbrahimMwakalinga <168201092+IbrahimMwakalinga@users.noreply.github.com>
2024-05-31 18:50:45 +0300

    Add files via upload

---
+++ Male.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package human;
+
+/**
+ *
+ * @author ENG.GIATUS MWIZARUBI
+ */
+class Male {
+    void display() {
+        System.out.println("Here are characteristics of Male \n");
+        System.out.println("Has short hair \n");
+        System.out.println("Has muscles and heavy voice \n");
+        System.out.println("Are shapeless \n");
+    }
+    
+}
